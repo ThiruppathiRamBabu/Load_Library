93fc704d9f3e7a8c62d3213fb587f9bd82601d7a
ThiruppathiRamBabu <snthiru2000@gmail.com>
2023-06-26 14:50:53 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const Jimp = require('jimp');
+
+function processImage(imagePath) {
+  Jimp.read(imagePath)
+    .then(image => {
+      image.resize(300, 200);
+      image.sepia();
+      image.write('output.jpg');
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+module.exports = {processImage};
